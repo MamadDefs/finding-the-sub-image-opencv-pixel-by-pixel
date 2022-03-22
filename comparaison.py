6aecac66c73c31d1400a24bed879bb9c1bf62bfd
Mohamadreza Habibzade <saoshyant1999@yahoo.com>
2022-03-22 21:27:33 +0430

    changing the comparaison min value

--- comparaison.py
+++ comparaison.py
@@ -11,17 +11,18 @@ crop_image = cv.imread("crop.jpg")
 originalHeight, originalWidth, originalZ = original.shape
 cropHeight, cropWidth, cropZ = crop_image.shape
 minI,minJ=0,0
-imageMin=3000
+imageMin=10000
 for i in range(originalHeight-cropHeight):
     for j in range(originalWidth-cropWidth):
         newCrop = original[i:i+cropHeight, j:j+cropWidth]
+        print(compare(crop_image,newCrop))
         if compare(crop_image, newCrop)<imageMin:
             imageMin=compare(crop_image,newCrop)
             minI=i
             minJ=j
 
-
-if imageMin!=3000: 
+print(imageMin)
+if imageMin!=10000: 
     cv.rectangle(original, (minJ + cropWidth, minI + cropHeight),(minJ, minI), (255, 0, 0), 5)
     cv.imshow("result", original)
 else:
--- crop.py
+++ crop.py
@@ -1,4 +1,4 @@
 import cv2 as cv
 
-original=cv.imread("1.jpg")
-cv.imwrite("crop.jpg",original[100:300,200:300])
+original=cv.imread("original.jpg")
+cv.imwrite("crop.jpg",original[100:400,200:300])
