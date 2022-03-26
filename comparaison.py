113918d96d80cfe9d06ebd472bec3a3295a18642
Mohamadreza Habibzade <saoshyant1999@yahoo.com>
2022-03-26 16:26:18 +0430

    test with sample image

--- comparaison.py
+++ comparaison.py
@@ -1,31 +1,36 @@
 import cv2 as cv
 
+
 def compare(original, crop):
     difference = cv.subtract(original, crop)
     b, g, r = cv.split(difference)
     return (cv.countNonZero(r)+cv.countNonZero(b)+cv.countNonZero(g))/3
 
-#main 
+
+# main
 original = cv.imread("original.jpg")
 crop_image = cv.imread("crop.jpg")
 originalHeight, originalWidth, originalZ = original.shape
 cropHeight, cropWidth, cropZ = crop_image.shape
-minI,minJ=0,0
-imageMin=10000
+minI, minJ = 0, 0
+imageMin = 10000
 for i in range(originalHeight-cropHeight):
     for j in range(originalWidth-cropWidth):
         newCrop = original[i:i+cropHeight, j:j+cropWidth]
-        print(compare(crop_image,newCrop))
-        if compare(crop_image, newCrop)<imageMin:
-            imageMin=compare(crop_image,newCrop)
-            minI=i
-            minJ=j
+        print(compare(crop_image, newCrop))
+        if compare(crop_image, newCrop) < imageMin:
+            imageMin = compare(crop_image, newCrop)
+            minI = i
+            minJ = j
 
 print(imageMin)
-if imageMin!=10000: 
-    cv.rectangle(original, (minJ + cropWidth, minI + cropHeight),(minJ, minI), (255, 0, 0), 5)
-    cv.imshow("result", original)
-else:
-    print("not found")
+cv.rectangle(original, (minJ + cropWidth, minI + cropHeight),
+             (minJ, minI), (255, 0, 0), 5)
+cv.imshow("result", original)
+# if imageMin!=10000:
+#     cv.rectangle(original, (minJ + cropWidth, minI + cropHeight),(minJ, minI), (255, 0, 0), 5)
+#     cv.imshow("result", original)
+# else:
+#     print("not found")
 
 cv.waitKey(0)
--- crop.py
+++ crop.py
@@ -1,4 +1,4 @@
 import cv2 as cv
 
-original=cv.imread("original.jpg")
-cv.imwrite("crop.jpg",original[100:400,200:300])
+original = cv.imread("original.jpg")
+cv.imwrite("crop.jpg", original[100:200, 200:300])
