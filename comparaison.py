836a7327ccec90265e1f5c717c8db3034c6481fe
Mohamadreza Habibzade <saoshyant1999@yahoo.com>
2022-05-07 19:03:39 +0430

    final
    performance

--- comparaison.py
+++ comparaison.py
@@ -1,6 +1,6 @@
 import cv2 as cv
-import numpy as np
-#compare function
+
+
 def compare(original, crop):
     difference = cv.subtract(original, crop)
     b, g, r = cv.split(difference)
@@ -13,24 +13,27 @@ crop_image = cv.imread("crop.jpg")
 originalHeight, originalWidth, originalZ = original.shape
 cropHeight, cropWidth, cropZ = crop_image.shape
 minI, minJ = 0, 0
-imageMin = 10000
+imageMin = 1100
+flag = False
 for i in range(originalHeight-cropHeight):
     for j in range(originalWidth-cropWidth):
         newCrop = original[i:i+cropHeight, j:j+cropWidth]
-        print(compare(crop_image, newCrop))
-        if compare(crop_image, newCrop) < imageMin:
+        #print(compare(crop_image, newCrop))
+        check = compare(crop_image, newCrop)
+        if check < imageMin:
             imageMin = compare(crop_image, newCrop)
             minI = i
             minJ = j
+            flag = True
+        if flag:
+            break
+    if flag:
+        break
 
 print(imageMin)
 cv.rectangle(original, (minJ + cropWidth, minI + cropHeight),
              (minJ, minI), (255, 0, 0), 5)
 cv.imshow("result", original)
-# if imageMin!=10000:
-#     cv.rectangle(original, (minJ + cropWidth, minI + cropHeight),(minJ, minI), (255, 0, 0), 5)
-#     cv.imshow("result", original)
-# else:
-#     print("not found")
+
 
 cv.waitKey(0)
